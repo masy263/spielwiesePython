160c2c6d16248aef6b28ee2badaa626b401b8782
Markus Symmank <markus.symmank@htwk-leipzig.de>
2024-11-27 13:43:57 +0100

    ...das Skript zur TC-Datenanalyse gemaess der aktuellen Anforderung fertigstellen.

--- fct_readRawTcData.py
+++ fct_readRawTcData.py
@@ -20,18 +20,15 @@ def fct_readRawTcData(inpFile):
 
     if asciiCmp == asciiData[idxChar - lenCmp:idxChar]:
       cutPos  = idxChar
-      print(cutPos)
       idxChar = lenData
 
     idxChar = idxChar + 1
 
   if cutPos > 0:
-    #print("found sof at char idx: %d" % cutPos)
-
     textData            = ''.join([chr(c) for c in asciiData[cutPos:]])
     payloadLengthLSByte = int(textData[0:2], 16)
     payloadLengthMSByte = int(textData[3:5], 16)
-    payloadLength = (payloadLengthMSByte * 256 + payloadLengthLSByte) * 3
+    payloadLength       = (payloadLengthMSByte * 256 + payloadLengthLSByte) * 3
 
     if len(textData) > payloadLength + 5:
       textData = textData[6:payloadLength + 5]
@@ -41,15 +38,24 @@ def fct_readRawTcData(inpFile):
     with open(inpFile+"_cut.hex", "w") as text_file:
       text_file.write("%s\n" % textData.replace(' ', '\n'))
 
-    textData = textData.split(' ')
-    decData = [int(cnt, 16) for cnt in textData]
-    strData = [str(cnt)+"\n" for cnt in decData]
-
-    with open(inpFile+"_cut.dec", "w") as text_file:
-      text_file.write("%s" % ''.join(strData))
-
-    ret = decData
   else:
-    print("[fct_readRawTcData] could not found sof in inp file")
+    print("[fct_readRawTcData] could not found sof in inp file...")
+    print("[fct_readRawTcData] assuming whole data is payload data...")
+    textData            = ''.join([chr(c) for c in asciiData])
+
+  while textData[0] == '' or textData[0] == ' ':
+    textData = textData[1:]
+
+  while textData[len(textData)-1] == '' or textData[len(textData)-1] == ' ':
+    textData = textData[0:len(textData)-2]
+
+  textData = textData.split(' ')
+  decData = [int(cnt, 16) for cnt in textData]
+  strData = [str(cnt)+"\n" for cnt in decData]
+
+  with open(inpFile+"_cut.dec", "w") as text_file:
+    text_file.write("%s" % ''.join(strData))
+
+  ret = decData
 
   return ret
--- script_241125_analyzeTcData.py
+++ script_241125_analyzeTcData.py
@@ -13,50 +13,127 @@
 #        :: max Magintude: 5905.876565
 #        :: rms:           1448.154730
 
+from fct_diy import *
 from fct_readRawTcData import *
+from fct_spectrum import *
 import os.path
 import numpy as np
+import matplotlib.pyplot as plt
+
+plt.style.use('dark_background')
+
 
 inpPath = '/home/markus/Arbeit/2024-11-21_testAufEvalBoard/tcOut241121agcAt50dB/'
 outPath = '/home/markus/Arbeit/2024-11-21_testAufEvalBoard/out/'
 
-minAgc       = 0
-maxAgc       = 100
-stpAgc       = 5
-idxAgc       = minAgc
-minGen       = -100
-maxGen       = 0
-stpGen       = 2
-idxGen       = minGen
-inpPrefixAdc = "tcAdc"
-#tcAdc_agc50dB_gen-31dBm
+minAgc        = 0
+maxAgc        = 100
+stpAgc        = 5
+idxAgc        = minAgc
+minGen        = -100
+maxGen        = 0
+stpGen        = 1
+idxGen        = minGen
+inpPrefixAdc  = "tcAdc"
+inpPrefixLVal = "tcLVals"
+infoFileAdc   = "infoAdc.dat"
+infoFileLVal  = "infoLVal.dat"
+
+with open(infoFileAdc, "w") as fid:
+  fid.write("   AGC  P_Tx -----------IMAG-------- -----------REAL-------- -----------CPLX--------       CFO Rel. to\n")
+  fid.write("  [dB] [dBm]    Min.    Max.     RMS    Min.    Max.     RMS    Min.    Max.     RMS      [Hz] ADC Max.\n")
+  fid.write("  <Ref.Data> %7d %7d %7d %7d %7d %7d %7d %7d %7d %9d\n" % (-5372,5460,1023,5452,5308,1024,0,5905,1448,192))
+
+with open(infoFileLVal, "w") as fid:
+  fid.write(" AGC. | Gen. | Min. | Max. | Amount Zeros | maxZeroRow\n")
+
 
 while idxAgc < maxAgc + stpAgc:
   idxGen = minGen
 
   while idxGen < maxGen + stpGen:
-    inpFileAdc = inpPath+inpPrefixAdc+"_agc"+str(idxAgc)+"dB_gen"+str(idxGen)+"dBm"
+    inpFileAdc  = inpPath+inpPrefixAdc+"_agc"+str(idxAgc)+"dB_gen"+str(idxGen)+"dBm"
+    inpFileLVal = inpPath+inpPrefixLVal+"_agc"+str(idxAgc)+"dB_gen"+str(idxGen)+"dBm"
 
     if os.path.isfile(inpFileAdc):
-      #print("[tb_241125_cutTcData] found file: %s" % inpFileAdc)
-      inpData  = fct_readRawTcData(inpFileAdc)
-      data     = np.array(inpData)
-      imag     = np.array(data[0::2])
-      real     = np.array(data[1::2])
-      cplx     = real + imag * 1j
-      print("Data ADC :: AGC: %ddB :: P_Tx: %ddBm" % (idxAgc, idxGen))
-      print("  >> IMAG :: min Magintude: %f" % np.min(imag))
-      print("  >>      :: max Magintude: %f" % np.max(imag))
-      print("  >>      :: rms:           %f" % np.sqrt(np.sum(imag**2) / len(imag)))
-      print("  >> REAL :: min Magintude: %f" % np.min(real))
-      print("  >>      :: max Magintude: %f" % np.max(real))
-      print("  >>      :: rms:           %f" % np.sqrt(np.sum(real**2) / len(real)))
-      print("  >> CPLX :: min Magintude: %f" % np.min(np.absolute(cplx)))
-      print("  >>      :: max Magintude: %f" % np.max(np.absolute(cplx)))
-      print("  >>      :: rms:           %f" % np.sqrt(np.sum(np.absolute(cplx)**2) / len(cplx)))
+      inpData    = fct_readRawTcData(inpFileAdc)
+      data       = np.array(inpData)
+      maxVal     = 0
+      imagMSByte = np.array(data[0::4])
+      imagLSByte = np.array(data[1::4])
+      imag       = imagMSByte * 256 + imagLSByte
+      imag       = fct_complementOnTwo2int(imag)
+      maxVal     = np.max(np.append(np.abs(imag), maxVal))
+      realMSByte = np.array(data[2::4])
+      realLSByte = np.array(data[3::4])
+      real       = realMSByte * 256 + realLSByte
+      real       = fct_complementOnTwo2int(real)
+      maxVal     = np.max(np.append(np.abs(real), maxVal))
+      cplx       = real + imag * 1j
+      cfoData    = cplx ** 4
+      spec       = fct_spectrum(cplx, 13500000, fftRes=256)
+      freqAxis   = spec[0]
+      specAxis   = spec[1]
+      cfo        = np.arange(0,len(specAxis))
+      cfo        = cfo * (specAxis == np.max(specAxis))
+      cfo        = np.max(cfo)
+      cfo        = freqAxis[cfo]
+
+      with open(infoFileAdc, "a") as fid:
+        fid.write(" %5d %5d" % (idxAgc, idxGen))
+        fid.write(" %7d %7d %7d" % (int(np.min(imag)), int(np.max(imag)), int(np.sqrt(np.sum(imag**2) / len(imag)))))
+        fid.write(" %7d %7d %7d" % (int(np.min(real)), int(np.max(real)), int(np.sqrt(np.sum(real**2) / len(real)))))
+        fid.write(" %7d %7d %7d" % (int(np.min(np.abs(cplx))), int(np.max(np.abs(cplx))), int(np.sqrt(np.sum(np.abs(cplx)**2) / len(cplx)))))
+        fid.write(" %9d" % cfo)
+        fid.write(" %7d\n" % int(maxVal / 2**15 *100))
+
+    if  os.path.isfile(inpFileLVal):
+      inpData = fct_readRawTcData(inpFileLVal)
+      tmp     = np.array(inpData)
+      lVal    = np.zeros(len(tmp) // 4)
+      idx     = 0
+
+      while idx < 4:
+        lVal = lVal + tmp[3-idx::4] * 256**idx
+        idx  = idx + 1
+
+      lVal       = (lVal - lVal % 2**9) / 2**9
+      lVal       = fct_complementOnTwo2int(lVal)
+      minLVal    = np.min(lVal)
+      maxLVal    = np.max(lVal)
+      cntZero    = 0
+      rowZero    = 0
+      maxRowZero = 0
+      idx        = 1
+
+      while idx < len(lVal):
+
+        if lVal[idx] == 0 and lVal[idx-1] == 0:
+          cntZero    = cntZero + 1
+          rowZero    = rowZero + 1
+          maxRowZero = max(rowZero, maxRowZero)
+        else:
+          rowZero    = 0
+
+        idx = idx + 2
+
+      with open(infoFileLVal, "a") as fid:
+        fid.write(" %4d | %4d |" % (idxAgc, idxGen))
+        fid.write(" %4d | %4d |        %5d |      %5d\n" % (minLVal, maxLVal, cntZero, maxRowZero))
 
     idxGen = idxGen + stpGen
 
   idxAgc = idxAgc + stpAgc
 
-#print(text)
+      #print("Data ADC :: AGC: %ddB :: P_Tx: %ddBm" % (idxAgc, idxGen))
+      #print("  >> IMAG :: min Magintude: %d" % int(np.min(imag)))
+      #print("  >>      :: max Magintude: %d" % int(np.max(imag)))
+      #print("  >>      :: rms:           %d" % int(np.sqrt(np.sum(imag**2) / len(imag))))
+      #print("  >> REAL :: min Magintude: %d" % int(np.min(real)))
+      #print("  >>      :: max Magintude: %d" % int(np.max(real)))
+      #print("  >>      :: rms:           %d" % int(np.sqrt(np.sum(real**2) / len(real))))
+      #print("  >> CPLX :: min Magintude: %d" % int(np.min(np.absolute(cplx))))
+      #print("  >>      :: max Magintude: %d" % int(np.max(np.absolute(cplx))))
+      #print("  >>      :: rms:           %d" % int(np.sqrt(np.sum(np.absolute(cplx)**2) / len(cplx))))
+      #print("  >>      :: rms:           %d" % int(np.sqrt(np.sum(np.absolute(cplx)**2) / len(cplx))))
+      #print("  >>      :: cfo:           %d" % int(cfo))
