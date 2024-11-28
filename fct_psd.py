67ff361e8ee123f6d598c65de4df65dedbf43c7c
Markus Symmank <markus.symmank@htwk-leipzig.de>
2024-11-28 16:03:31 +0100

    ...mit einer PSD-Funktion beginnen, um die spaeter den Signal-CFO zu schaetzen.

---
+++ fct_psd.py
@@ -0,0 +1,44 @@
+# determine spectrum including resolution ans axis
+
+import numpy as np
+
+def fct_spectrum(sig, fSamp, fftRes=1024):
+  sig   = sig * np.hanning(len(sig))
+  spec  = np.fft.fft(sig, n=fftRes)
+  spec  = np.fft.fftshift(10 * np.log10(np.abs(spec)**2))
+  t     = np.arange(0, len(spec)) * len(spec) / len(sig) / fSamp
+  fAxis = np.fft.fftfreq(t.shape[-1]) * fSamp
+  fAxis = np.fft.fftshift(fAxis)
+  #spec  = np.append(spec[len(spec) // 2:], spec[0:len(spec) // 2])
+  #fAxis = (np.arange(0,fftRes) / fftRes - 0.5) * fSamp
+  ret   = np.array([fAxis, spec])
+
+  return ret
+
+# testbench
+
+# import matplotlib.pyplot as plt
+
+# plt.style.use('dark_background')
+
+# fSamp = 100
+
+# t = np.arange(0, 32, 1 / fSamp)
+# f = 0.1
+# a = 10
+# vz = 1
+# sig = a * np.sin(2*np.pi*f*t)
+
+# while f < 20:
+#   f = f + 0.02
+#   a = a + vz * 0.5
+#   if a < 0:
+#     vz = vz * -1
+#   sig = sig + a * np.sin(2*np.pi*f*t)
+
+# spec = fct_spectrum(sig, fSamp, 16)
+# xAxis = spec[0]
+# yAxis = spec[1]
+
+# plt.plot(xAxis, yAxis)
+# plt.show()
